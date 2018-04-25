6d223a820d02038124de49dc3ee996781e8f7369
Marcin Świrek <swirek.marcin@gmail.com>
2018-04-26 00:34:35 +0200

    Add document.ready method in js file

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 //JS
 
-// $(function() {
-//   var url = "http://api.icndb.com/jokes/random";
-//   var jokeText = document.getElementById("joke-text");
-//   var button = document.getElementById("random-joke");
+$(function() {
+  var url = "http://api.icndb.com/jokes/random";
+  var jokeText = document.getElementById("joke-text");
+  var button = document.getElementById("random-joke");
 
-//   button.addEventListener("click", function() {
-//     getJoke();
-//   });
+  button.addEventListener("click", function() {
+    getJoke();
+  });
 
-//   function getJoke() {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", url);
-//     xhr.addEventListener("load", function() {
-//       var response = JSON.parse(xhr.response);
-//       jokeText.innerHTML = response.value.joke;
-//     });
-//     xhr.send();
-//   }
-// });
+  function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.addEventListener("load", function() {
+      var response = JSON.parse(xhr.response);
+      jokeText.innerHTML = response.value.joke;
+    });
+    xhr.send();
+  }
+  $(document).ready(function() {
+    getJoke();
+  });
+});
 
 //jQuery
 
-$(function() {
-  var url = "http://api.icndb.com/jokes/random";
-  var $button = $("#random-joke").click(function() {
-    getJoke();
-  });
-  var $jokeText = $("#joke-text");
+// $(function() {
+//   var url = "http://api.icndb.com/jokes/random";
+//   var $button = $("#random-joke").click(function() {
+//     getJoke();
+//   });
+//   var $jokeText = $("#joke-text");
 
-  function getJoke() {
-    $.ajax({
-      method: "GET",
-      url: url,
-      success: function(res) {
-        $jokeText.text(res.value.joke);
-      }
-    });
-  }
-});
+//   function getJoke() {
+//     $.ajax({
+//       method: "GET",
+//       url: url,
+//       success: function(res) {
+//         $jokeText.text(res.value.joke);
+//       }
+//     });
+//   }
+// });
