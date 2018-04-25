02d7f3ee2bc5e0bba2b5942cb763a03b68261d9f
Marcin Swirek <swirek.marcin@gmail.com>
2018-04-25 20:17:02 +0200

    Add code in html, css and js files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  var url = "http://api.icndb.com/jokes/random";
+  var jokeText = document.getElementById("joke-text");
+  var button = document.getElementById("random-joke");
+
+  button.addEventListener("click", function() {
+    getJoke();
+  });
+
+  function getJoke() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.addEventListener("load", function() {
+      var response = JSON.parse(xhr.response);
+      jokeText.innerHTML = response.value.joke;
+    });
+    xhr.send();
+  }
+});
