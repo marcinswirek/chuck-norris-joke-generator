ff99bd17725e466ca81ea0dfb629e9581e74b342
Marcin Swirek <swirek.marcin@gmail.com>
2018-04-27 11:41:17 +0200

    Replace jQuery with vanila JS in .js file

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
 //JS
 
-$(function() {
-  var url = "http://api.icndb.com/jokes/random";
-  var jokeText = document.getElementById("joke-text");
-  var button = document.getElementById("random-joke");
+var url = "http://api.icndb.com/jokes/random";
+var jokeText = document.getElementById("joke-text");
+var button = document.getElementById("random-joke");
 
-  button.addEventListener("click", function() {
-    getJoke();
-  });
-
-  function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.addEventListener("load", function() {
-      var response = JSON.parse(xhr.response);
-      jokeText.innerHTML = response.value.joke;
-    });
-    xhr.send();
-  }
-  $(document).ready(function() {
-    getJoke();
-  });
+button.addEventListener("click", function() {
+  getJoke();
 });
 
-//jQuery
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.addEventListener("load", function() {
+    var response = JSON.parse(xhr.response);
+    jokeText.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
 
+document.addEventListener("DOMContentLoaded", getJoke());
+
+
+//jQuery
+//0;
 // $(function() {
 //   var url = "http://api.icndb.com/jokes/random";
 //   var $button = $("#random-joke").click(function() {
