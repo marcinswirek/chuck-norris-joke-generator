1c3dad04b62b134a65ca006349a4ed8ab2de7f82
Marcin Swirek <swirek.marcin@gmail.com>
2018-09-20 21:49:08 +0200

    Replace jQuery with JS

--- js/script.js
+++ js/script.js
@@ -19,24 +19,3 @@ function getJoke() {
 }
 
 document.addEventListener("DOMContentLoaded", getJoke());
-
-
-//jQuery
-//0;
-// $(function() {
-//   var url = "http://api.icndb.com/jokes/random";
-//   var $button = $("#random-joke").click(function() {
-//     getJoke();
-//   });
-//   var $jokeText = $("#joke-text");
-
-//   function getJoke() {
-//     $.ajax({
-//       method: "GET",
-//       url: url,
-//       success: function(res) {
-//         $jokeText.text(res.value.joke);
-//       }
-//     });
-//   }
-// });
